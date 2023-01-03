99ba4916ca5bcf5e8e2918e14d6071c60b79f150
Leonam Rabelo <53281220+LeonamRabelo@users.noreply.github.com>
2023-01-03 15:58:07 -0300

    Add files via upload

---
+++ Estruturas de Dados/Pilha/Questao5.java
@@ -0,0 +1,47 @@
+package Pilha;
+
+import Estruturas.*;
+import java.util.Scanner;
+
+public class Questao5{
+    public static void main(String[] args){
+        Scanner str = new Scanner(System.in);
+        System.out.println("Digite a expressão: ");
+        String expressao = str.nextLine();
+
+        //String expressao = "{2 + [(16 + 2 ∧ 3) - 4)} * 3"; //para teste caso necessario
+        Stack<Character> s = new Stack<Character>();
+        
+        for(int i = 0; i < expressao.length(); i++){
+            Character var = expressao.charAt(i);
+
+            if(var == '{' || var == '[' || var == '('){
+                s.push(var);
+            }else if(var == '}' || var == ']' || var == ')'){
+                if(var == '}'){
+                    var = '{';
+                }else if(var == ']'){
+                    var = '[';
+                }else if(var == ')'){
+                    var = '(';
+                }
+
+                if(s.isEmpty()){
+                    System.out.printf("Invalido\n\n");
+                    System.exit(-1);
+                }else if(var.compareTo(s.stackTop()) == 0){
+                    s.pop();
+                }else{
+                    System.out.printf("Invalido\n\n");
+                    System.exit(-1);
+                }
+            }
+        }
+
+        if(s.isEmpty()){
+            System.out.printf("Valido\n\n");
+        }else{
+            System.out.printf("Invalido\n\n");
+        }
+    }
+}
